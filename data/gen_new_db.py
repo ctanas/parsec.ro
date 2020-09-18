dd3ab7113d578e287331ebea2b57631dedefb8b0
Claudiu <claudiu@tanaselia.ro>
2020-09-18 22:03:01 +0300

    update

--- data/gen_new_db.py
+++ data/gen_new_db.py
@@ -12,7 +12,6 @@ print("launchDate TIMESTAMP,")
 print("lv_type TEXT,")
 print("lv_serial TEXT,")
 print("lv_Payload TEXT,")
-print("sat_orbitClass TEXT,")
 print("ls_state TEXT,")
 print("lv_name TEXT,")
 print("lv_launchPad TEXT,")
@@ -24,11 +23,11 @@ with open(txt_input) as fp:
     line_lv = fp.readline()
     cnt=1
     while line_lv:
-        if len(line_lv)<157: bulno = ""
-        else: bulno = line_lv[156:158].strip()
+        if len(line_lv)<156: bulno = ""
+        else: bulno = line_lv[156:len(line_lv)].strip()
         if len(bulno) == 2: 
             bulno = "0"+bulno
             bulno = "[[/bul/"+bulno+"]["+str(int(bulno))+"]]"
-        print("INSERT INTO launches VALUES ('" + line_lv[0:11].strip() +"','" + line_lv[11:22].strip() + "','" + line_lv[11:29].strip() +"','" + line_lv[29:52].strip().replace("'","''") + "','"+ line_lv[52:72].strip() + "','" + line_lv[72:117].strip().replace("'","''") + "','" + line_lv[117:127].strip() + "','" + line_lv[127:132].strip() + "','" + line_lv[132:144].strip() + "','" + line_lv[144:154].strip() + "','" + line_lv[154:156].strip() + "','" + bulno + "');")
+        print("INSERT INTO launches VALUES ('" + line_lv[0:12].strip() +"','" + line_lv[12:23].strip() + "','" + line_lv[12:31].strip() +"','" + line_lv[31:55].strip().replace("'","''") + "','"+ line_lv[55:76].strip() + "','" + line_lv[76:123].strip().replace("'","''") + "','" + line_lv[123:129].strip() + "','" + line_lv[129:142].strip() + "','" + line_lv[142:153].strip() + "','" + line_lv[153:155].strip() + "','" + bulno + "');")
         line_lv = fp.readline()
         cnt+=1
